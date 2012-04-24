b84b16e614dafd070ef039e5db1e0497e6db4450
Max Penet <max.penet@gmail.com>
2012-04-24 14:41:38 +0200

    prepare release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zilch "0.1.0-SNAPSHOT"
+(defproject org.clojars.mpenet/zilch "0.1.0-SNAPSHOT"
   :description "Zilch: A Clojure 0MQ Messaging Library"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.zeromq/zmq "2.0-SNAPSHOT"]]
